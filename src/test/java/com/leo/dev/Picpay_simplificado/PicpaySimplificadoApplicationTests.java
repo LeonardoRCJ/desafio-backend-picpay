2af833c32ea837ab6ec00990601422cc657c19c1
Leonardo Rocha <leonardorocha790@gmail.com>
2024-11-04 12:03:27 -0300

    feat: Project commit

---
+++ src/test/java/com/leo/dev/Picpay_simplificado/PicpaySimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.dev.Picpay_simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
